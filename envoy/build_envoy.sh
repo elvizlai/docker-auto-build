f94263944016978ba2adae21eefa69501cba1241
elvizlai <sdrzlyz@gmail.com>
2021-04-28 13:05:16 +0800

    envoy: upgrade

--- envoy/build_envoy.sh
+++ envoy/build_envoy.sh
@@ -1,12 +1,14 @@
 # TODO remove if exist dir envoy-src
-git clone -b v1.17.1 --depth 1 https://github.com/envoyproxy/envoy envoy-src
-
-cd envoy-src
 
 # need if GFW:
 # export https_proxy=http://192.168.3.63:7890 http_proxy=http://192.168.3.63:7890 all_proxy=socks5://192.168.3.63:7890
 
+git clone -b v1.18.2 --depth 1 https://github.com/envoyproxy/envoy envoy-src
+
+cd envoy-src
+
 export ENVOY_DOCKER_BUILD_DIR=~/tmp/envoy-docker-build/
 
-./ci/run_envoy_docker.sh 'BAZEL_BUILD_EXTRA_OPTIONS="--define exported_symbols=enabled" ./ci/do_ci.sh bazel.release.server_only'
-# ./ci/run_envoy_docker.sh 'BAZEL_BUILD_EXTRA_OPTIONS="--define exported_symbols=enabled" ./ci/do_ci.sh bazel.sizeopt.server_only'
+# genedated file: ~/tmp/envoy-docker-build/
+./ci/run_envoy_docker.sh 'BAZEL_BUILD_EXTRA_OPTIONS="--define exported_symbols=enabled" ./ci/do_ci.sh bazel.sizeopt.server_only'
+# ./ci/run_envoy_docker.sh 'BAZEL_BUILD_EXTRA_OPTIONS="--define exported_symbols=enabled" ./ci/do_ci.sh bazel.release.server_only'
