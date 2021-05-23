b39df3b53783a92c6ef469f376d1e09f3d767843
elvizlai <sdrzlyz@gmail.com>
2021-05-23 15:24:46 +0800

    feat: nginx ffmpeg static

--- nginx/ffmpeg.sh
+++ nginx/ffmpeg.sh
@@ -5,13 +5,17 @@
 
 echo "add ffmpeg static file"
 
+curl -sSL https://x.morn.io/dl/ffmpeg/ffmpeg > /usr/bin/ffmpeg
+curl -sSL https://x.morn.io/dl/ffmpeg/ffprobe > /usr/bin/ffprobe
+chmod +x /usr/bin/ffmpeg /usr/bin/ffprobe
+
 # 4.x
-cd /tmp
-curl -sSL https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
-tar -xf ffmpeg.tar.xz -C . --strip-components 1
-\cp ffmpeg /usr/bin/ffmpeg
-\cp ffprobe /usr/bin/ffprobe
-rm -rf /tmp/*
+# cd /tmp
+# curl -sSL https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
+# tar -xf ffmpeg.tar.xz -C . --strip-components 1
+# \cp ffmpeg /usr/bin/ffmpeg
+# \cp ffprobe /usr/bin/ffprobe
+# rm -rf /tmp/*
 
 # location /live_stat {
 #     rtmp_stat all;
