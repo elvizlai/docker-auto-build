618ca0d163d883bae7d69cbf56264b327ccdc6b6
elvizlai <sdrzlyz@gmail.com>
2021-11-02 14:36:16 +0800

    feat: pgroonga

---
+++ pg/_curl.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function __curl() {
+  read proto server path <<<$(echo ${1//// })
+  DOC=/${path// //}
+  HOST=${server//:*}
+  PORT=${server//*:}
+  [[ x"${HOST}" == x"${PORT}" ]] && PORT=80
+
+  exec 3<>/dev/tcp/${HOST}/$PORT
+  echo -en "GET ${DOC} HTTP/1.0\r\nHost: ${HOST}\r\n\r\n" >&3
+  (while read line; do
+   [[ "$line" == $'\r' ]] && break
+  done && cat) <&3
+  exec 3>&-
+}
+
+__curl $1
