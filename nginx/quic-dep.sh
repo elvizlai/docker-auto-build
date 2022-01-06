b8479ffd372fb7587776ba70130d0318d996b003
elvizlai <sdrzlyz@gmail.com>
2022-01-06 09:36:42 +0800

    upgrade

--- nginx/quic-dep.sh
+++ nginx/quic-dep.sh
@@ -12,8 +12,8 @@ source scl_source enable devtoolset-9 || true
 PCRE=pcre-8.45
 ZLIB=zlib-1.2.11
 JEMALLOC=5.2.1
-LUAJIT=v2.1-20210510
-LUAROCKS=3.7.0
+LUAJIT=v2.1-20211210
+LUAROCKS=3.8.0
 
 mkdir -p /opt/lib-src && cd /opt/lib-src
 
