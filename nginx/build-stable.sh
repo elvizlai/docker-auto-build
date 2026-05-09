f70b60d459ba37aabc99358770c02542d4c605b4
elvizlai <sdrzlyz@gmail.com>
2026-05-09 09:51:50 +0800

    nginx: module update

--- nginx/build-stable.sh
+++ nginx/build-stable.sh
@@ -63,8 +63,8 @@ NGINXVER=${1:-1.30.0}
 NGINXNJS=0.9.8
 NGINXDIR=/opt/nginx-$NGINXVER
 NGINXNDK=0.3.4
-NGINXLUA=0.10.31rc1
-NGINXSTREAMLUA=0.0.19rc1
+NGINXLUA=0.10.31rc2
+NGINXSTREAMLUA=0.0.19rc2
 
 mkdir -p $NGINXDIR/module && cd $NGINXDIR/module
 
@@ -206,7 +206,7 @@ mkdir -p /var/cache/nginx/client_temp /var/log/nginx /etc/nginx/conf.d /etc/ngin
 cd /etc/nginx/lualib
 
 # https://github.com/openresty/lua-resty-core/tags
-LUA_RESTY_CORE=0.1.34rc1
+LUA_RESTY_CORE=0.1.34rc2
 curl -sSL https://github.com/openresty/lua-resty-core/archive/v$LUA_RESTY_CORE.tar.gz | tar zxf -
 \cp -rf lua-resty-core-$LUA_RESTY_CORE/lib/* .
 rm -rf lua-resty-core-$LUA_RESTY_CORE
@@ -236,7 +236,7 @@ curl -sSL https://github.com/openresty/lua-resty-redis/archive/v$LUA_RESTY_REDIS
 rm -rf lua-resty-redis-$LUA_RESTY_REDIS
 
 # https://github.com/openresty/lua-resty-string/tags
-LUA_RESTY_STRING=0.16
+LUA_RESTY_STRING=0.17
 curl -sSL https://github.com/openresty/lua-resty-string/archive/v$LUA_RESTY_STRING.tar.gz | tar zxf -
 \cp -rf lua-resty-string-$LUA_RESTY_STRING/lib/* .
 rm -rf lua-resty-string-$LUA_RESTY_STRING
@@ -248,7 +248,7 @@ curl -sSL https://github.com/openresty/lua-resty-upload/archive/v$LUA_RESTY_UPLO
 rm -rf lua-resty-upload-$LUA_RESTY_UPLOAD
 
 # https://github.com/openresty/lua-resty-upstream-healthcheck/tags
-LUA_RESTY_UPSTREAM_HEALTHCHECK=0.08
+LUA_RESTY_UPSTREAM_HEALTHCHECK=0.09
 curl -sSL https://github.com/openresty/lua-resty-upstream-healthcheck/archive/v$LUA_RESTY_UPSTREAM_HEALTHCHECK.tar.gz | tar zxf -
 \cp -rf lua-resty-upstream-healthcheck-$LUA_RESTY_UPSTREAM_HEALTHCHECK/lib/* .
 rm -rf lua-resty-upstream-healthcheck-$LUA_RESTY_UPSTREAM_HEALTHCHECK
@@ -260,7 +260,7 @@ curl -sSL https://github.com/openresty/lua-resty-websocket/archive/v$LUA_RESTY_W
 rm -rf lua-resty-websocket-$LUA_RESTY_WEBSOCKET
 
 # https://github.com/openresty/lua-cjson/tags
-LUA_CJSON=2.1.0.16
+LUA_CJSON=2.1.0.17
 curl -sSL https://github.com/openresty/lua-cjson/archive/$LUA_CJSON.tar.gz | tar zxf -
 LUA_INCLUDE_DIR=/usr/local/include/luajit-2.1 make -C lua-cjson-$LUA_CJSON
 mv -f lua-cjson-$LUA_CJSON/cjson.so .
@@ -297,7 +297,7 @@ curl -sSL https://github.com/bungle/lua-resty-template/archive/v$LUA_RESTY_TPL.t
 rm -rf lua-resty-template-$LUA_RESTY_TPL
 
 # https://github.com/leafo/pgmoon/tags
-LUA_PGMOON=1.16.0
+LUA_PGMOON=1.17.0
 curl -sSL https://github.com/leafo/pgmoon/archive/v$LUA_PGMOON.tar.gz | tar zxf -
 \cp -rf pgmoon-$LUA_PGMOON/pgmoon .
 rm -rf pgmoon-$LUA_PGMOON
